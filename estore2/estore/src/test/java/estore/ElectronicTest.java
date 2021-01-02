75386f776a4c9aedb83936404eed77f16ea8199d
Dinesh Sainath <32024572+DineshSainath@users.noreply.github.com>
2021-01-02 18:54:02 -0500

    Add files via upload
    Added file functionalities

---
+++ estore2/estore/src/test/java/estore/ElectronicTest.java
@@ -0,0 +1,42 @@
+//
+//package estore;
+//
+//import eStoreSearch.Electronic;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+//public class ElectronicTest {
+//    @Test public void testSetAndGetMaker() {
+//        Electronic e = new Electronic();
+//        e.setMaker("Apple");
+//        assertEquals("Maker should be Apple", "Apple",e.getMaker());   
+//    }
+//
+//        //To check productID, price, year and description from the super class - product through the subclass - Electronic
+//
+//        @Test public void testSetandGetProductID() {
+//            Electronic e = new Electronic();
+//            e.setProductID(01);
+//            assertEquals("Product ID should be: 01",01,e.getProductID());
+//        }
+//    
+//        @Test public void testSetandGetPrice() {
+//            Electronic e = new Electronic();
+//            e.setPrice(90);
+//            assertEquals("Price should be: 90",90,e.getPrice());
+//        }
+//    
+//        @Test public void testSetandGetYear() {
+//            Electronic e = new Electronic();
+//            e.setYear(2005);
+//            assertEquals("Year should be: 2005",2005,e.getYear());
+//        }
+//    
+//        @Test public void testSetandGetDescription() {
+//            Electronic e = new Electronic();
+//            e.setDescription("iPhone SE");
+//            assertEquals("Description should be: iPhone SE","iPhone SE",e.getDescription());
+//        }
+//        
+//}
+//
